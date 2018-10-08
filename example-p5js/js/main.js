fd59a859432fb48e4e4faddaa3d73ba5a48ffb7d
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-07 22:31:21 -0400

    add libraries

--- example-p5js/js/main.js
+++ example-p5js/js/main.js
@@ -1,7 +1,14 @@
 "use strict";
 
+var bvh;
+
+function preload() {
+    bvh = loadStrings("./files/pirouette.bvh");
+}
+
 function setup() {
 	createCanvas(800, 600);
+	console.log(bvh);
 }
 
 function draw() {
