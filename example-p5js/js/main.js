1cf39d42548e6ebbd131e046f3e929296572d464
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-08 11:23:58 -0400

    camera works

--- example-p5js/js/main.js
+++ example-p5js/js/main.js
@@ -1,16 +1,45 @@
 "use strict";
 
-var bvh;
+var canvas, bvh, easycam;
 
 function preload() {
-    bvh = loadStrings("./files/pirouette.bvh");
+    bvh = new BVHLoader("./files/pirouette.bvh");
 }
 
 function setup() {
-	createCanvas(800, 600);
-	console.log(bvh);
+  pixelDensity(1);
+  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+  setAttributes('antialias', true);
+
+  easycam = createEasyCam();
+  bvh.parse();
+} 
+
+function draw(){
+	rotateX(-0.5);
+	rotateY(-0.5);
+	scale(10);
+  
+  	background(50, 0, 50);
+	strokeWeight(1);
+
+	fill(255, 64, 0);
+	box(15);
+  
+	push();
+	translate(0, 0, 20);
+	fill(0, 64, 255);
+	box(5);
+	pop();
+  
+  	push();
+	translate(0, 0, -20);
+	fill(64, 255, 0);
+	box(5);
+	pop();
 }
 
-function draw() {
-	background(50, 0, 50);
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+  easycam.setViewport([0,0,windowWidth, windowHeight]);
 }
