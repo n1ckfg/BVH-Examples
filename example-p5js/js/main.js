d34dc462251beb5bfec463c080f271d88a712c90
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-09 22:06:57 -0400

    Update example-p5js/js/main.js

--- example-p5js/js/main.js
+++ example-p5js/js/main.js
@@ -2,30 +2,36 @@
 
 var canvas, easycam, counter, fps;
 var bvh, skeleton;
+var rotOffset;
 
 function preload() {
-    bvh = new BVHLoader("./files/pirouette.bvh");
+    bvh = new BVHLoader("./files/Brekel_03_11_2016_15_47_42_body1.bvh");
 }
 
 function setup() {
-  pixelDensity(1);
   canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  setAttributes('antialias', true);
+  pixelDensity(1);
+  setAttributes("antialias", true);
 
   easycam = createEasyCam();
   skeleton = new Skeleton(bvh);
   counter = 0;
   fps = 1.0 / 30.0;
+  rotOffset = createVector(-0.5, -0.5, 0);
 } 
 
+function rotate3D(rot) {
+	rotateX(rot.x);
+	rotateY(rot.y);
+	rotateZ(rot.z);
+}
+
 function draw(){
-	rotateX(-0.5);
-	rotateY(-0.5);
+	rotate3D(rotOffset);
 	scale(10);
   
   	background(50, 0, 50);
 	strokeWeight(1);
-	fill(255, 64, 0);
 
 	var time = parseFloat(millis()) / 1000.0;
 	var lastTime = 0;
@@ -36,12 +42,28 @@ function draw(){
 			var bone = limb.bones[j];
 			var frame = bone.frames[counter];
 			var position = frame.position;
-			var rotation = frame.rotation;
+			var rotation = frame.rotation.toEuler();
 			lastTime = frame.time;
 		
 			if (j === 0) push();
 			translate(position.x + bone.offset.x, position.y + bone.offset.y, position.z + bone.offset.z);
-			box(1);
+			rotate3D(rotation);
+			if (bone.name === "Head") {
+				fill(10, 64, 255);
+				box(4);
+			} else if (bone.name === "Hips") {
+				fill(255);
+				box(4);
+			} else if (bone.name === "LeftHand") {
+				fill(10, 255, 64);
+				box(4);
+			} else if (bone.name === "RightHand") {
+				fill(255, 64, 10);
+				box(4);
+			} else {
+				fill(155);
+				box(2);
+			}
 			if (j === limb.bones.length - 1) pop();
 		}
 	}
