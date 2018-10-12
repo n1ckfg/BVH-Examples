96dbdf256b59d83f1620a03da810c54c8ddad126
Nick Fox-Gieg <nick@fox-gieg.com>
2018-10-12 10:23:45 -0400

    new bvh library

--- example-p5js/js/main.js
+++ example-p5js/js/main.js
@@ -1,26 +1,25 @@
 "use strict";
 
 var canvas, easycam, counter, fps;
-var bvh, skeleton;
+var motion, skel;
 var rotOffset;
-var lastTime = 0;
+var url = "./files/Jackson.bvh";
+var ready = false;
 
 function preload() {
-    bvh = new BVHLoader("./files/Brekel_03_11_2016_15_47_42_body1.bvh");
-    //bvh = new BVHLoader("./files/Jackson.bvh");
-    //bvh = new BVHLoader("./files/pirouette.bvh");
+	BVH.read(url, function(motion) {
+		skel = new Skeleton(motion);
+		ready = true;
+	});	
 }
 
 function setup() {
-  canvas = createCanvas(windowWidth, windowHeight, WEBGL);
-  pixelDensity(1);
-  setAttributes("antialias", true);
+    canvas = createCanvas(windowWidth, windowHeight, WEBGL);
+    pixelDensity(1);
+    setAttributes("antialias", true);
 
-  easycam = createEasyCam();
-  skeleton = new Skeleton(bvh);
-  counter = 0;
-  fps = 1.0 / 30.0;
-  rotOffset = createVector(-0.5, -0.5, 0);
+    easycam = createEasyCam();
+    rotOffset = createVector(-0.5, -0.5, 0);
 } 
 
 function rotate3D(rot) {
@@ -29,58 +28,139 @@ function rotate3D(rot) {
 	rotateZ(rot.z);
 }
 
-function draw(){
+function draw() {
 	rotate3D(rotOffset);
 	scale(10);
   
   	background(50, 0, 50);
 	strokeWeight(1);
 
-	var time = parseFloat(millis()) / 1000.0;
-
-	for (var i=0; i<skeleton.bones.length; i++) {
-		var bone = skeleton.bones[i];
-		drawBone(bone);
+	if (ready) {
+		skel.update();
 	}
-
-	if (time > lastTime + fps) {
-		if (counter < skeleton.frameCount - 1) {
-			counter ++;
-		} else {
-			counter = 0;
-		}
-	}
-}
-
-function drawBone(bone) {
-	var frame = bone.frames[counter];
-	lastTime = frame.time;
-	var position = frame.position;
-	var rotation = frame.rotation.toEuler();
-	
-	push();
-	translate(position.x + bone.offset.x, position.y + bone.offset.y, position.z + bone.offset.z);
-	rotate3D(rotation);
-	if (bone.name === "Head") {
-		fill(10, 64, 255);
-		box(4);
-	} else if (bone.name === "Hips") {
-		fill(255);
-		box(4);
-	} else if (bone.name === "LeftHand") {
-		fill(10, 255, 64);
-		box(4);
-	} else if (bone.name === "RightHand") {
-		fill(255, 64, 10);
-		box(4);
-	} else {
-		fill(155);
-		box(2);
-	}
-	pop();
 }
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
   easycam.setViewport([0,0,windowWidth, windowHeight]);
+}
+
+class Skeleton {
+
+	constructor(motion) {
+		this.motion = motion;
+		this.fps = 1.0 / this.motion.frameTime;
+	  	this.frameCount = this.motion.numFrames;
+		this.lastTime = 0;
+  		this.counter = 0;
+  		this.bones = [];
+
+		for (var i=0; i<this.motion.nodeList.length; i++) {
+			this.bones.push(this.motion.of(this.motion.nodeList[i].id));
+		}
+		
+		console.log("____________________________________");
+		console.log("bones: " + this.bones.length + "   frames: " + this.frameCount + "   fps: " + this.fps);
+	}
+
+	drawBone(bone) {
+		bone.at[this.counter];
+		var position = createVector(bone.offsetX, bone.offsetY, bone.offsetZ);
+		
+		push();
+		translate(position.x, position.y, position.z);
+		if (bone.id === "Head") {
+			fill(10, 64, 255);
+			box(4);
+		} else if (bone.id === "Hips") {
+			fill(255);
+			box(4);
+		} else if (bone.id === "LeftHand") {
+			fill(10, 255, 64);
+			box(4);
+		} else if (bone.id === "RightHand") {
+			fill(255, 64, 10);
+			box(4);
+		} else {
+			fill(155);
+			box(2);
+		}
+		pop();
+	}
+
+	drawBones() {
+		for (var i=0; i<this.bones.length; i++) {
+				var bone = this.bones[i];
+				this.drawBone(bone);
+		}
+	}
+
+	update() {
+		this.time = parseFloat(millis()) / 1000.0;
+
+		if (this.time > this.lastTime + this.fps) {
+			if (this.counter < this.frameCount - 1) {
+				this.counter ++;
+			} else {
+				this.counter = 0;
+			}
+			this.lastTime = this.time;
+		}
+
+		this.drawBones();
+	}
+
+}
+
+// ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ 
+
+function test() {
+	var url = "./files/Jackson.bvh";
+
+	BVH.read(url, function(motion) {
+	  // basic infomation about motion data
+	  console.log("fps: " + 
+	  1.0 / motion.frameTime + "   frames: " +
+	  motion.numFrames + "\nnodes: \n" +
+
+	  // get lists of nodes
+	  motion.nodeList);
+
+	  var id = "Neck";
+	  var index = 4;
+
+	  // get a node by id
+	  var node = motion.of(id)
+
+	  // change node's internal state to n-th frame
+	  node.at(index)
+
+	  // you can exchange the order of method "at" and "of"
+	  var state = motion.at(index)
+	  node = state.of(id)
+
+	  // node properties
+	  node.offsetX
+	  node.offsetY
+	  node.offsetZ
+	  node.rotationX
+	  node.rotationY
+	  node.rotationZ
+
+	  // node adjacent to "End Site" has properties about endOffset
+	  if (node.hasEnd) {
+	    node.endOffsetX
+	    node.endOffsetY
+	    node.endOffsetZ
+	  }
+
+	  console.log("\"" + id + "\"" + " frame " + index + ": pos (" + 				  
+	  node.offsetX + ", " +
+	  node.offsetY + ", " +
+	  node.offsetZ + "), rot (" +
+	  node.rotationX + ", " +
+	  node.rotationY + ", " +
+	  node.rotationZ + ")");
+
+	});
 }
